5426edd6f1e0c5497a914114a5cbc870c07c0707
Spencer S <spencerschaper@gmail.com>
2023-02-04 23:31:13 -0500

    Update transcript.ts

--- utils/transcript.ts
+++ utils/transcript.ts
@@ -28,7 +28,7 @@ export default function transcript(interaction: ExtendedInteraction | ExtendedBu
         // Execute transcript generation
         let transcriptSetup: string
         if (os === 'windows') transcriptSetup = `cd ./dce && DiscordChatExporter.Cli.exe export -t ${process.env.TOKEN} -b -c ${interaction.channelId} -o ../transcripts/${UUID}.html`
-        if (os === 'linux') transcriptSetup = `cd ./dce && mono DiscordChatExporter.Cli.exe export -t ${process.env.TOKEN} -b -c ${interaction.channelId} -o ../transcripts/${UUID}.html`
+        if (os === 'linux') transcriptSetup = `cd ./dce && mono DiscordChatExporter.Cli.dll export -t ${process.env.TOKEN} -b -c ${interaction.channelId} -o ../transcripts/${UUID}.html`
 
 
         await runCommand(transcriptSetup!)
